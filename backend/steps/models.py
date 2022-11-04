aecd58820007ccd4fb3f82c19ec7df6d452e26b2
zut3 <z.gl3b@yandex.ru>
2022-11-04 10:52:26 +0300

    added condition field to step model

--- backend/steps/models.py
+++ backend/steps/models.py
@@ -5,4 +5,5 @@ from tests.models import Test
 class Step(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=128, blank=True)
+    condition = models.JSONField(null=False, verbose_name="условие задания в 'pretty' формате") # rendering at frontend
     test = models.ForeignKey(Test, on_delete=models.CASCADE)
